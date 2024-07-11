a1335e6772eca38ce67f9420458b26c55d5da0b7
Its-premmine <pprakashdumra@gmail.com>
2024-07-11 13:54:24 +0530

    logout butnon setup

--- src/page/MyPage.jsx
+++ src/page/MyPage.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./MyPage.scss"
+import { useNavigate } from "react-router-dom";
 
 const MyPage = () => {
+    const navigate = useNavigate();
+    const handleLogout = () => {
+        navigate('/')
+    }
     return(
         <div >
             
             <div className="headearTag">
             
             <img src="https://framerusercontent.com/images/ba9osFXOxT2jz5pagZ7lDhnZDw.svg" alt="" className="chiImg" />
-            <input type="button" value="Logout" className="headerButton"/>
+            <input type="button" value="Logout" className="headerButton" onClick={handleLogout}/>
             </div>
             <div className="mainBody">
                 <div className="mybody">
